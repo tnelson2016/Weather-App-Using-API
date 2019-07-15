cf1a01d32fee539faf84c7479bf0ae8cd2cc67f6
Your Name <you@example.com>
2019-07-15 16:00:22 -0400

    Updates

---
+++ precache-manifest.cf0769429d81c606876caf0165e1121f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93c9d7817bef6a6270aeb5f5cdff6268",
+    "url": "/Weather-App-Using-API/index.html"
+  },
+  {
+    "revision": "b7d0a1c52a2637a03f99",
+    "url": "/Weather-App-Using-API/static/css/2.20eb5eb3.chunk.css"
+  },
+  {
+    "revision": "733f6d56f7be36b39a9f",
+    "url": "/Weather-App-Using-API/static/css/main.3f13b3a5.chunk.css"
+  },
+  {
+    "revision": "b7d0a1c52a2637a03f99",
+    "url": "/Weather-App-Using-API/static/js/2.9b3ead1c.chunk.js"
+  },
+  {
+    "revision": "733f6d56f7be36b39a9f",
+    "url": "/Weather-App-Using-API/static/js/main.45b54826.chunk.js"
+  },
+  {
+    "revision": "3972620d9fb494768106",
+    "url": "/Weather-App-Using-API/static/js/runtime~main.811349c2.js"
+  },
+  {
+    "revision": "a0e2fd4e6ec4be05fb659a52e311378c",
+    "url": "/Weather-App-Using-API/static/media/bg.a0e2fd4e.jpg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/Weather-App-Using-API/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/Weather-App-Using-API/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/Weather-App-Using-API/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/Weather-App-Using-API/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/Weather-App-Using-API/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
